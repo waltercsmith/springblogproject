448575daaa862f69437ac8afc0f9a50758e9e2b1
Walter Smith <walter.c.smith01@gmail.com>
2021-04-01 12:58:08 -0400

    Feat:Create an interface for posts named PostRepository that extends JpaRepository

--- src/main/java/com/codeup/springblog/models/Ad.java
+++ src/main/java/com/codeup/springblog/models/Ad.java
@@ -1,4 +1,16 @@
 package com.codeup.springblog.models;
 
+import javax.persistence.*;
+
+@Entity
 public class Ad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String Title;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String Description;
 }
---
+++ src/main/java/com/codeup/springblog/repos/AdRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.springblog.repos;
+
+import com.codeup.springblog.models.Ad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdRepository extends JpaRepository<Ad, Long> {
+}
