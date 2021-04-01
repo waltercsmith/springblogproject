6879d6af82e8690796226d02cc0c3f8ca820e3ac
Walter Smith <walter.c.smith01@gmail.com>
2021-04-01 08:13:36 -0400

    Feat:Created views for controllers

---
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -0,0 +1,24 @@
+package com.codeup.springblog.models;
+
+public class Post {
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    private String title;
+    private String body;
+
+}
